05500a107a5a7cb1bf55d538be33280ab158a4a6
Shuming Cui <1005586890@qq.com>
2022-06-16 14:20:25 +0800

    delete published packages when fail

--- cli.js
+++ cli.js
@@ -5,6 +5,7 @@ const lib = require('./lib.js');
 const argv = require('yargs/yargs')(process.argv.slice(2))
   .option('token', { description: 'token', type: 'string' })
   .option('owner', { description: 'owner', type: 'string' })
+  .option('repo', { description: 'repo', type: 'string' })
   .help().argv;
 
 lib.rollbackRelease(argv).catch(console.error);
