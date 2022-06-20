2195e9933ffefd7084fb0f36e154e8e3064c29aa
Shuming Cui <1005586890@qq.com>
2022-06-20 16:49:37 +0800

    test create new pr

--- cli.js
+++ cli.js
@@ -6,6 +6,8 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
   .option('token', { description: 'token', type: 'string' })
   .option('owner', { description: 'owner', type: 'string' })
   .option('repo', { description: 'repo', type: 'string' })
+  .option('baseRef', { description: 'repo', type: 'string' })
+  .option('headRef', { description: 'repo', type: 'string' })
   .help().argv;
 
 lib.rollbackRelease(argv).catch(console.error);
